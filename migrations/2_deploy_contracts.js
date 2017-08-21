86d583730f9f29caa0416fb173064e074ec56b01
JeremyJiaqg <185356122@qq.com>
2017-08-21 16:48:48 +0800

    changed deploy time

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,14 +1,26 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var Math = artifacts.require("./SafeMath.sol");
 var WanChainICO = artifacts.require("./WanchainContribution.sol");
+var wanTokenOwner = '0x21e7f6c5fe0e26609f271f6e9f1343bb753f1096';
+var year = 2017;
+var month = 8;
+var date = 21;
+var hour = 17;
+var minute = 0;
+var second = 0;
+
 
 module.exports = function(deployer,network, accounts) {
-  var timenow = new Date().getTime();
+
+  var timenow = new Date(year,month,date,hour,minute,second).getTime();
+
+
   var left = timenow % 1000;
   var timeseconds = (timenow - left) / 1000;
   deployer.deploy(ConvertLib);
   deployer.link(ConvertLib, [WanChainICO]);
   deployer.deploy(Math);
   deployer.link(Math, [WanChainICO]);
-  deployer.deploy(WanChainICO, accounts[0], timeseconds);
+  deployer.deploy(WanChainICO,wanTokenOwner, timeseconds);
+
 };
