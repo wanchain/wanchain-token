65cfe9854ef1e4479be620123d7d5434cdce386b
JeremyJiaqg <185356122@qq.com>
2017-08-25 10:11:25 +0800

    added rospten node

--- truffle.js
+++ truffle.js
@@ -39,12 +39,14 @@ module.exports = {
       gasPrice: 20e9,
       from: "0xf93df8c288b9020e76583a6997362e89e0599e99",
     },
-    // ropsten: {
-    //   network_id: 3,
-    //   provider: providerRopsten,
-    //   gas: 4000000,
-    //   gasPrice: 20e9,
-    // },
+    ropsten: {
+      host: "114.215.69.125",
+      port: 8545,
+      network_id: 4,
+      gas: 4000000,
+      gasPrice: 20e9,
+      from: "0x2d0e7c0813a51d3bd1d08246af2a8a7a57d8922e",
+     },
     rinkeby: {
       host: "118.190.71.27",
       port: 8545,
