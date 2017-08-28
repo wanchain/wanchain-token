55c2a1b64fabeeb5e9d07e93c0b7e1f1276305fa
JeremyJiaqg <185356122@qq.com>
2017-08-28 13:31:32 +0800

    changed deploy configure for eth main net

--- truffle.js
+++ truffle.js
@@ -19,25 +19,13 @@ module.exports = {
       from: "0xf93df8c288b9020e76583a6997362e89e0599e99",
     },
 
-    live: {
-      host: "178.25.19.88",
-      port: 80,
-      network_id: 1,
-      // optional config values:
-      // gas
-      // gasPrice
-      // from - default address to use for any transaction Truffle makes during migrations
-      // provider - web3 provider instance Truffle should use to talk to the Ethereum network.
-      //          - if specified, host and port are ignored.
-    },
-
     mainnet: {
-      network_id: 1,
-      host: "https://mainnet.infura.io/metamask",
-      port: 80,
+      host: "118.190.71.27",
+      port: 8745,
+      network_id: "*", // Match any network id
       gas: 4000000,
       gasPrice: 20e9,
-      from: "0xf93df8c288b9020e76583a6997362e89e0599e99",
+      from:"0x96fe33e72f758114de310a38d871696519cccf62",
     },
     ropsten: {
       host: "114.215.69.125",
