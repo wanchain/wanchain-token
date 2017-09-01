9ca1851b4d8fe4e119ef93026b26be8759f70ca8
JeremyJiaqg <185356122@qq.com>
2017-09-01 13:20:33 +0800

    updated ICO time

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,25 +1,22 @@
 var ConvertLib = artifacts.require("./ConvertLib.sol");
 var Math = artifacts.require("./SafeMath.sol");
 var WanChainICO = artifacts.require("./WanchainContribution.sol");
+
 var wanTokenOwner = '0x21e7f6c5fe0e26609f271f6e9f1343bb753f1096';
 
 module.exports = function(deployer,network, accounts) {
 
-    var timenow = new Date('2017/09/06 20:00:00').getTime();
-    var left = timenow % 1000;
-    var timeseconds = (timenow - left) / 1000;
+  var dateTime = new Date('2017/09/06 20:00:00');
+  var left = dateTime % 1000;
+  var timeseconds = (dateTime - left) / 1000;
 
-    console.log(timeseconds);
+  console.log(dateTime.toUTCString());
+  console.log(timeseconds);
 
-    // var timenow1 = new Date().getTime();
-    // var left1 = timenow1% 1000;
-    // var timeseconds1 = (timenow1- left1) / 1000;
-    // console.log(timeseconds1);
-
-    deployer.deploy(ConvertLib);
-    deployer.link(ConvertLib, [WanChainICO]);
-    deployer.deploy(Math);
-    deployer.link(Math, [WanChainICO]);
-    deployer.deploy(WanChainICO,wanTokenOwner, timeseconds);
+  deployer.deploy(ConvertLib);
+  deployer.link(ConvertLib, [WanChainICO]);
+  deployer.deploy(Math);
+  deployer.link(Math, [WanChainICO]);
+  deployer.deploy(WanChainICO,wanTokenOwner, timeseconds);
 
 };
